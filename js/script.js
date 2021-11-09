12f48388849753cec9e3ec65db0b07163778a2d6
weresoti <weresoti@mail.ru>
2021-11-09 18:54:42 +0300

    testimonials

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@ document.querySelector('.header__burger').onclick = function() {
    document.querySelector('.header__burger').classList.toggle('active')
    $('body').toggleClass('lock')
 }
+
+new Swiper ('.swiper-container',{
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	/*centeredSlides: true,*/
+	spaceBetween: 20,
+	slidesPerView: "auto",
+	watchOverflow: true,
+	initialSlide: 0,
+	/*loop: true,*/
+	autoplay: {
+		delay: 5000,
+		stopOnLastSlide: false,
+		disableOnIteration: false,
+	},
+   /*effect: 'flip',
+   flipEffect: {
+      slideShadows: true,
+      limitRotation: true,
+   }*/
+});
