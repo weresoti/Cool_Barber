138cc95c98e509e27b87b643991517f5b8ad869f
weresoti <weresoti@mail.ru>
2021-11-13 18:11:41 +0300

    added form validation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-/*document.querySelector('.header__burger').onclick = function() {
-   document.querySelector('.header__menu').classList.toggle('header__menu-active')
-   document.querySelector('.header__burger').classList.toggle('active')
-   $('body').toggleClass('lock')
-}*/
+const inputPhone = document.querySelector(".get__form__input-phone")
+const inputEmail = document.querySelector(".get__form__input-email")
+const checkBox = document.querySelector("input[type=checkbox]");
+const checkBoxLabel = document.querySelector(".get__form__checkbox__label");
+
 document.addEventListener("click", function (event) {
   if (event.target.closest(".header__burger")) {
     document.querySelector(".header__menu").classList.toggle("header__menu-active");
@@ -14,7 +14,61 @@ document.addEventListener("click", function (event) {
 		document.querySelector(".header__burger").classList.remove("active")
 		document.querySelector("body").classList.remove("lock")
 	}
+	if (event.target.closest(".get__form__btn")) {
+    if (inputPhone.value =='') {
+      document.getElementById("error1").innerHTML ="<p>Required *</p>"
+      inputPhone.style.border = "1px solid #E05263"
+      event.preventDefault();
+    }
+    if (inputEmail.value =='') {
+      document.getElementById("error2").innerHTML ="<p>Required *</p>"
+      inputEmail.style.border = "1px solid #E05263"
+      event.preventDefault();
+    }
+    if (!checkBox.checked) {
+      checkBoxLabel.style.color = "#E05263";
+      event.preventDefault()
+    }
+    if ((!phoneTest(inputPhone)) || (!emailTest(inputEmail)) || (!checkBox.checked)) {
+      event.preventDefault()
+    }
+    if ((phoneTest(inputPhone)) && (emailTest(inputEmail)) && (checkBox.checked)) {
+
+    }
+	}
 });
+
+inputPhone.addEventListener("input", function (event) {
+  if (phoneTest(inputPhone)) {
+    document.getElementById("error1").innerHTML ="<p></p>"
+    inputPhone.style.border = "1px solid #4FE18A"
+  }
+  if (!phoneTest(inputPhone)) {
+    document.getElementById("error1").innerHTML ="<p>Error</p>"
+    document.getElementById("error2").style.marginTop = "5px"
+    inputPhone.style.border = "1px solid #E05263"
+  }
+})
+
+inputEmail.addEventListener("input", function (event) {
+  if (emailTest(inputEmail)) {
+    document.getElementById("error2").innerHTML ="<p></p>"
+    inputEmail.style.border = "1px solid #4FE18A"
+  }
+  if (!emailTest(inputEmail)) {
+    document.getElementById("error2").innerHTML ="<p>Error</p>"
+    document.getElementById("error2").style.marginTop = "5px"
+    inputEmail.style.border = "1px solid #E05263"
+  }
+})
+
+function phoneTest(inputPhone) {
+  return /^[(+]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{4}[-\s\.]{0,1}[0-9]{4}$/.test(inputPhone.value);
+}
+
+function emailTest(inputEmail) {
+	return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(inputEmail.value);
+}
 
 new Swiper(".swiper-container", {
   navigation: {
